cfe6d6df94c13207a8bf30f40df302b1e9ceadb9
Taevon Jordan <taevonjordan1@gmail.com>
2020-02-28 22:10:35 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -8,4 +8,10 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
+  i = 0
+  movies = directors_database[0][:movies]
+  while i < movies.count do
+    
+    i += 1
+  end
 end
